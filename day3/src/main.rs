f064cc1e121a9658c96095ed2672cf6426b91663
Christian Mandery <christian@mandery.ch>
2023-12-03 19:24:58 +0100

    Finish day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,13 @@
 use anyhow::Result;
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::fs::read_to_string;
 use std::path::Path;
+
+lazy_static! {
+    // Compile regex only once
+    static ref NUMBERS_REGEX: Regex = Regex::new("\\d+").unwrap();
+}
 
 /// Extracts the sum of all numbers that are adjacent to the given field, if this field contains a symbol.
 fn calc_adjacent_number_sum(f: &[Vec<u8>], row: usize, col: usize) -> Option<u32> {
@@ -28,12 +34,43 @@ fn calc_adjacent_number_sum(f: &[Vec<u8>], row: usize, col: usize) -> Option<u32
     }
 }
 
-fn calc_field_result(f: &[Vec<u8>]) -> u32 {
+/// Extracts the product of two adjacent numbers if this field contains a * symbol (second part of the puzzle).
+fn calc_adjacent_gear_ratio(f: &[Vec<u8>], row: usize, col: usize) -> Option<u32> {
+    let c = f[row][col];
+
+    if c as char == '*' {
+        let mut numbers = vec![];
+
+        numbers.append(&mut get_adjacent_numbers_from_row(&f[row], col));
+
+        if row != 0 {
+            numbers.append(&mut get_adjacent_numbers_from_row(&f[row - 1], col));
+        }
+
+        if row != f.len() - 1 {
+            numbers.append(&mut get_adjacent_numbers_from_row(&f[row + 1], col));
+        }
+
+        if numbers.len() == 2 {
+            return Some(numbers.first().unwrap() * numbers.get(1).unwrap());
+        }
+    }
+
+    None
+}
+
+fn calc_field_result(f: &[Vec<u8>], compute_gear_ratio: bool) -> u32 {
     let mut sum = 0;
 
     for row in 0..f.len() {
         for col in 0..f[row].len() {
-            if let Some(value) = calc_adjacent_number_sum(f, row, col) {
+            let res = if compute_gear_ratio {
+                calc_adjacent_gear_ratio(f, row, col)
+            } else {
+                calc_adjacent_number_sum(f, row, col)
+            };
+
+            if let Some(value) = res {
                 sum += value;
             }
         }
@@ -44,10 +81,9 @@ fn calc_field_result(f: &[Vec<u8>]) -> u32 {
 
 /// Returns every number in the row that is adjacent to the given column index
 fn get_adjacent_numbers_from_row(row: &[u8], col_index: usize) -> Vec<u32> {
-    let r = Regex::new("\\d+").unwrap(); // regex recompiled every time, could be optimized
     let mut numbers = vec![];
 
-    for m in r.find_iter(&String::from_utf8(row.to_owned()).unwrap()) {
+    for m in NUMBERS_REGEX.find_iter(&String::from_utf8(row.to_owned()).unwrap()) {
         if m.start() <= col_index + 1 && m.end() >= col_index {
             numbers.push(m.as_str().parse().unwrap());
         }
@@ -58,9 +94,15 @@ fn get_adjacent_numbers_from_row(row: &[u8], col_index: usize) -> Vec<u32> {
 
 fn main() -> Result<()> {
     let field = read_input_file("../inputs/day3_input.txt")?;
+
     println!(
         "Sum of all numbers that adjacent to symbols: {}",
-        calc_field_result(&field)
+        calc_field_result(&field, false)
+    );
+
+    println!(
+        "Sum of all gear ratios: {}",
+        calc_field_result(&field, true)
     );
 
     Ok(())
@@ -79,6 +121,12 @@ mod tests {
     #[test]
     fn example_first_star() {
         let field = read_input_file("../inputs/day3_example.txt").unwrap();
-        assert_eq!(calc_field_result(&field), 4361);
+        assert_eq!(calc_field_result(&field, false), 4361);
+    }
+
+    #[test]
+    fn example_second_star() {
+        let field = read_input_file("../inputs/day3_example.txt").unwrap();
+        assert_eq!(calc_field_result(&field, true), 467835);
     }
 }
