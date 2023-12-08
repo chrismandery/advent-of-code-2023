19dfcd5fa666603d37a6ac9f096fad303a283e5d
Christian Mandery <christian@mandery.ch>
2023-12-08 11:19:19 +0100

    Solve second part of day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use num::integer::lcm;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs::read_to_string;
@@ -9,12 +10,14 @@ struct Input {
     network_map: HashMap<String, (String, String)>,
 }
 
-fn count_steps(input: &Input) -> Result<usize> {
+fn count_steps(input: &Input, start_pos: &str, terminate_on_z_suffix: bool) -> Result<usize> {
     let mut step_count = 0;
-    let mut cur_pos = "AAA";
+    let mut cur_pos = start_pos;
     let mut cur_dir = input.directions.iter().cycle();
 
-    while cur_pos != "ZZZ" {
+    while (!terminate_on_z_suffix && cur_pos != "ZZZ")
+        || (terminate_on_z_suffix && !cur_pos.ends_with('Z'))
+    {
         let map = input
             .network_map
             .get(cur_pos)
@@ -32,10 +35,43 @@ fn count_steps(input: &Input) -> Result<usize> {
     Ok(step_count)
 }
 
+fn count_steps_parallel(input: &Input) -> Result<usize> {
+    let step_counts: Vec<Result<usize>> = input
+        .network_map
+        .keys()
+        .filter_map(|pos| {
+            if pos.ends_with('A') {
+                Some(count_steps(input, pos, true))
+            } else {
+                None
+            }
+        })
+        .collect();
+    let step_counts: Result<Vec<usize>> = step_counts.into_iter().collect();
+
+    // Due to the way the input is constructed (the distance from an A-suffixed start position to the first Z-suffixed end position is
+    // always the same as the distance from that end position to the next end position), we can just calculate the least common multiple
+    // of all the step counts.
+    let lcm = step_counts?
+        .into_iter()
+        .reduce(lcm)
+        .context("Could not determine LCM!")?;
+
+    Ok(lcm)
+}
+
 fn main() -> Result<()> {
     let input = read_input_file("../inputs/day8_input.txt")?;
 
-    println!("Number of steps to reach ZZZ: {}", count_steps(&input)?);
+    println!(
+        "Number of steps to reach ZZZ from AAA: {}",
+        count_steps(&input, "AAA", false)?
+    );
+
+    println!(
+        "Number of steps to reach Z-suffixed node from all start nodes: {}",
+        count_steps_parallel(&input)?
+    );
 
     Ok(())
 }
@@ -61,7 +97,7 @@ fn read_input_file<P: AsRef<Path>>(input_path: P) -> Result<Input> {
     lines.next();
 
     // Read mappings in network map
-    let re = Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
+    let re = Regex::new(r"([12A-Z]{3}) = \(([12A-Z]{3}), ([12A-Z]{3})\)").unwrap();
     let mut network_map = HashMap::new();
 
     for line in lines {
@@ -90,12 +126,18 @@ mod tests {
     #[test]
     fn example1() {
         let input = read_input_file("../inputs/day8_example1.txt").unwrap();
-        assert_eq!(count_steps(&input).unwrap(), 2);
+        assert_eq!(count_steps(&input, "AAA", false).unwrap(), 2);
     }
 
     #[test]
     fn example2() {
         let input = read_input_file("../inputs/day8_example2.txt").unwrap();
-        assert_eq!(count_steps(&input).unwrap(), 6);
+        assert_eq!(count_steps(&input, "AAA", false).unwrap(), 6);
+    }
+
+    #[test]
+    fn example3() {
+        let input = read_input_file("../inputs/day8_example3.txt").unwrap();
+        assert_eq!(count_steps_parallel(&input).unwrap(), 6);
     }
 }
