73c45fd7bf6e3f6c9161704d30889dde065b9b00
Christian Mandery <christian@mandery.ch>
2023-12-22 00:59:00 +0100

    Day 17 solution using Dijkstra algorithm

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,143 +1,160 @@
 use anyhow::Result;
 use array2d::Array2D;
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::path::Path;
 
 type Position = (usize, usize);
 type Field = Array2D<u8>;
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct State {
+    position: Position,
+    last_move_was_vertical: Option<bool>,
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct MapPath {
-    positions: Vec<Position>,
-    directions: Vec<Direction>,
-    cost: usize,
-    heuristics_to_goal: usize,
-}
-
-impl Ord for MapPath {
-    fn cmp(&self, other: &Self) -> Ordering {
-        (other.cost + other.heuristics_to_goal).cmp(&(self.cost + self.heuristics_to_goal))
-    }
-}
-
-impl PartialOrd for MapPath {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-/// Calculates the optimal path under the given constraints using the A* algorithm. We are using the Manhattan distance to the end position
-/// as heuristics for the A* algorithm (and assume cost of only 1 for each field). Since the BinaryHeap in Rust's standard library is a max
-/// heap, the Ord implementation for MapPath above is inverted. Note that the returned path does not include the start position.
-fn calculate_optimal_path(f: &Field) -> MapPath {
+/// Calculates the optimal path under the given constraints using the Djikstra algorithm. Note that the returned path does not include the
+/// start position.
+fn calculate_optimal_path_cost(f: &Field, step_size_min: usize, step_size_max: usize) -> usize {
     let rows0 = f.num_rows() - 1;
     let columns0 = f.num_columns() - 1;
 
-    let mut open_paths = BinaryHeap::new();
+    let mut open_paths = HashMap::new();
 
-    let initial_path = MapPath {
-        positions: vec![],
-        directions: vec![],
-        cost: 0,
-        heuristics_to_goal: 2 * (rows0 + columns0),
+    let initial_path = State {
+        position: (0, 0),
+        last_move_was_vertical: None,
     };
-    open_paths.push(initial_path);
+    open_paths.insert(initial_path, 0);
 
-    while let Some(cur_path) = open_paths.pop() {
-        let last_pos = cur_path.positions.last().copied().unwrap_or((0, 0));
-        let last_dir = cur_path
-            .directions
-            .last()
-            .copied()
-            .unwrap_or(Direction::Right);
+    loop {
+        // Get path with minimal cost
+        let (last_state, last_cost) = open_paths
+            .iter()
+            .min_by_key(|(_, cost)| *cost)
+            .map(|(k, v)| (k.clone(), *v))
+            .unwrap();
+        open_paths.remove(&last_state);
 
         /* println!(
-            "Cur path @ {}/{} ({} total positions), cost of {}, heuristic is {}, cost+heuristics is {}",
-            last_pos.0,
-            last_pos.1,
-            cur_path.positions.len(),
-            cur_path.cost,
-            cur_path.heuristics_to_goal,
-            cur_path.cost + cur_path.heuristics_to_goal
+            "At {}/{} with cost {}",
+            last_state.position.0, last_state.position.1, last_cost
         ); */
 
-        if last_pos == (rows0, columns0) {
-            return cur_path;
+        if last_state.position == (rows0, columns0) {
+            return last_cost;
         }
 
-        // TODO: Check last three elements and determine possibly forbidden direction
+        if !last_state.last_move_was_vertical.unwrap_or(false) {
+            // Go up
+            let mut added_cost = 0;
 
-        // Go up
-        if last_pos.0 != 0 && last_dir != Direction::Down {
-            let new_pos = (last_pos.0 - 1, last_pos.1);
+            for i in 1..=step_size_max {
+                if last_state.position.0 < i {
+                    break;
+                }
 
-            let mut p = cur_path.clone();
-            p.positions.push(new_pos);
-            p.directions.push(Direction::Up);
-            p.cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
-            p.heuristics_to_goal = 2 * ((rows0 - new_pos.0) + (columns0 - new_pos.1));
+                let new_pos = (last_state.position.0 - i, last_state.position.1);
+                added_cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
 
-            open_paths.push(p);
+                if i >= step_size_min {
+                    let s = State {
+                        position: new_pos,
+                        last_move_was_vertical: Some(true),
+                    };
+
+                    let old_cost = *open_paths.get(&s).unwrap_or(&usize::MAX);
+                    let new_cost = old_cost.min(last_cost + added_cost);
+                    open_paths.insert(s, new_cost);
+                }
+            }
+
+            // Go down
+            let mut added_cost = 0;
+
+            for i in 1..=step_size_max {
+                if last_state.position.0 + i > rows0 {
+                    break;
+                }
+
+                let new_pos = (last_state.position.0 + i, last_state.position.1);
+                added_cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
+
+                if i >= step_size_min {
+                    let s = State {
+                        position: new_pos,
+                        last_move_was_vertical: Some(true),
+                    };
+
+                    let old_cost = *open_paths.get(&s).unwrap_or(&usize::MAX);
+                    let new_cost = old_cost.min(last_cost + added_cost);
+                    open_paths.insert(s, new_cost);
+                }
+            }
         }
 
-        // Go down
-        if last_pos.0 != rows0 && last_dir != Direction::Up {
-            let new_pos = (last_pos.0 + 1, last_pos.1);
+        if last_state.last_move_was_vertical.unwrap_or(true) {
+            // Go left
+            let mut added_cost = 0;
 
-            let mut p = cur_path.clone();
-            p.positions.push(new_pos);
-            p.directions.push(Direction::Down);
-            p.cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
-            p.heuristics_to_goal = 2 * ((rows0 - new_pos.0) + (columns0 - new_pos.1));
+            for i in 1..=step_size_max {
+                if last_state.position.1 < i {
+                    break;
+                }
 
-            open_paths.push(p);
-        }
+                let new_pos = (last_state.position.0, last_state.position.1 - i);
+                added_cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
 
-        // Go left
-        if last_pos.1 != 0 && last_dir != Direction::Right {
-            let new_pos = (last_pos.0, last_pos.1 - 1);
+                if i >= step_size_min {
+                    let s = State {
+                        position: new_pos,
+                        last_move_was_vertical: Some(false),
+                    };
 
-            let mut p = cur_path.clone();
-            p.positions.push(new_pos);
-            p.directions.push(Direction::Left);
-            p.cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
-            p.heuristics_to_goal = 2 * ((rows0 - new_pos.0) + (columns0 - new_pos.1));
+                    let old_cost = *open_paths.get(&s).unwrap_or(&usize::MAX);
+                    let new_cost = old_cost.min(last_cost + added_cost);
+                    open_paths.insert(s, new_cost);
+                }
+            }
 
-            open_paths.push(p);
-        }
+            // Go right
+            let mut added_cost = 0;
 
-        // Go right
-        if last_pos.1 != columns0 && last_dir != Direction::Left {
-            let new_pos = (last_pos.0, last_pos.1 + 1);
+            for i in 1..=step_size_max {
+                if last_state.position.1 + i > columns0 {
+                    break;
+                }
 
-            let mut p = cur_path.clone();
-            p.positions.push(new_pos);
-            p.directions.push(Direction::Right);
-            p.cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
-            p.heuristics_to_goal = 2 * ((rows0 - new_pos.0) + (columns0 - new_pos.1));
+                let new_pos = (last_state.position.0, last_state.position.1 + i);
+                added_cost += *f.get(new_pos.0, new_pos.1).unwrap() as usize;
 
-            open_paths.push(p);
+                if i >= step_size_min {
+                    let s = State {
+                        position: new_pos,
+                        last_move_was_vertical: Some(false),
+                    };
+
+                    let old_cost = *open_paths.get(&s).unwrap_or(&usize::MAX);
+                    let new_cost = old_cost.min(last_cost + added_cost);
+                    open_paths.insert(s, new_cost);
+                }
+            }
         }
     }
-
-    panic!("No solution found - this should never happen!");
 }
 
 fn main() -> Result<()> {
     let f = read_input_file("../inputs/day17_input.txt")?;
 
-    let path = calculate_optimal_path(&f);
-    println!("Heat loss on optimal path (first star): {}", path.cost);
+    println!(
+        "Heat loss on optimal path first star: {}",
+        calculate_optimal_path_cost(&f, 1, 3)
+    );
+
+    println!(
+        "Heat loss on optimal path for second star: {}",
+        calculate_optimal_path_cost(&f, 4, 10)
+    );
 
     Ok(())
 }
@@ -163,9 +180,15 @@ fn read_input_file<P: AsRef<Path>>(input_path: P) -> Result<Field> {
 mod tests {
     use super::*;
 
-    /* #[test]
+    #[test]
     fn example_first_star() {
         let f = read_input_file("../inputs/day17_example.txt").unwrap();
-        assert_eq!(calculate_optimal_path(&f).cost, 102);
-    } */
+        assert_eq!(calculate_optimal_path_cost(&f, 1, 3), 102);
+    }
+
+    #[test]
+    fn example_second_star() {
+        let f = read_input_file("../inputs/day17_example.txt").unwrap();
+        assert_eq!(calculate_optimal_path_cost(&f, 4, 10), 94);
+    }
 }
